b0c72c5c155be02ded1e856f39b0a407354f60da
Shamsul Amry Mokhtar <amry@equadtech.com>
2015-04-08 11:03:32 +0800

    Can't use HttpClient for HTTPS. Too lazy to figure out so using RestSharp instead.

--- Amry.Gst.Core/GstWebScraper.cs
+++ Amry.Gst.Core/GstWebScraper.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Net.Http;
+using System.Net;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
+using RestSharp;
 
 namespace Amry.Gst
 {
-    public class GstWebScraper : IGstDataSource, IDisposable
+    public class GstWebScraper : IGstDataSource
     {
-        readonly HttpClient _client = new HttpClient() {
-            BaseAddress = new Uri("https://gst.customs.gov.my/TAP/")
+        readonly RestClient _client = new RestClient("https://gst.customs.gov.my/TAP/") {
+            CookieContainer = new CookieContainer()
         };
 
         GstLookupInputType? _inputType;
         bool _isInitialized;
         string _token;
-
-        public void Dispose()
-        {
-            _client.Dispose();
-        }
 
         public async Task<IList<GstLookupResult>> LookupGstData(GstLookupInputType inputType, string input)
         {
@@ -42,79 +39,85 @@ namespace Amry.Gst
 
         async Task InitializeToken()
         {
-            var resp = await _client.GetAsync("GetWlbToken", HttpCompletionOption.ResponseHeadersRead);
+            var req = new RestRequest("GetWlbToken");
+            var resp = await _client.ExecuteGetTaskAsync(req);
             UpdateTokenForNextRequest(resp);
         }
 
         async Task LoadFrontPage()
         {
-            var resp = await _client.GetAsync("_/?Load=1&FAST_VERLAST__" + _token, HttpCompletionOption.ResponseHeadersRead);
+            var req = new RestRequest("_/");
+            req.AddParameter("Load", 1);
+            req.AddParameter("FAST_VERLAST__", _token);
+            var resp = await _client.ExecuteGetTaskAsync(req);
             UpdateTokenForNextRequest(resp);
         }
 
         async Task BrowseToLookupPage()
         {
-            var formData = new Dictionary<string, string> {
-                {"DOC_MODAL_ID__", "0"},
-                {"EVENT__", "b-i"},
-                {"FAST_VERLAST__", _token}
-            };
-            var resp = await _client.PostAsync("_/EventOccurred", new FormUrlEncodedContent(formData));
+            var req = new RestRequest("_/EventOccurred");
+            req.AddParameter("DOC_MODAL_ID__", 0);
+            req.AddParameter("EVENT__", "b-i");
+            req.AddParameter("FAST_VERLAST__", _token);
+            var resp = await _client.ExecutePostTaskAsync(req);
             UpdateTokenForNextRequest(resp);
         }
 
         async Task SelectLookupInputType(GstLookupInputType inputType)
         {
-            var formData = new Dictionary<string, string> {
-                {"DOC_MODAL_ID__", "0"},
-                {"FAST_VERLAST__", _token}
-            };
+            var req = new RestRequest("_/Recalc");
 
             switch (inputType) {
                 case GstLookupInputType.GstNumber:
-                    formData.Add("d-3", "true");
+                    req.AddParameter("d-3", "true");
                     break;
 
                 case GstLookupInputType.BusinessRegNumber:
-                    formData.Add("d-6", "true");
+                    req.AddParameter("d-6", "true");
                     break;
 
                 case GstLookupInputType.BusinessName:
-                    formData.Add("d-8", "true");
+                    req.AddParameter("d-8", "true");
                     break;
             }
 
-            var resp = await _client.PostAsync("_/Recalc", new FormUrlEncodedContent(formData));
+            req.AddParameter("DOC_MODAL_ID__", 0);
+            req.AddParameter("FAST_VERLAST__", _token);
+            var resp = await _client.ExecutePostTaskAsync(req);
             UpdateTokenForNextRequest(resp);
             _inputType = inputType;
         }
 
         async Task<IList<GstLookupResult>> ExecuteLookup(string input)
         {
-            var formData = new Dictionary<string, string> {
-                {"DOC_MODAL_ID__", "0"},
-                {"FAST_VERLAST__", _token}
-            };
+            var req = new RestRequest("_/Recalc");
 
             switch (_inputType) {
                 case GstLookupInputType.GstNumber:
-                    formData.Add("d-5", input);
+                    req.AddParameter("d-5", input);
                     break;
 
                 case GstLookupInputType.BusinessRegNumber:
-                    formData.Add("d-7", input);
+                    req.AddParameter("d-7", input);
                     break;
 
                 case GstLookupInputType.BusinessName:
-                    formData.Add("d-9", input);
+                    req.AddParameter("d-9", input);
                     break;
             }
 
-            var resp = await _client.PostAsync("_/Recalc", new FormUrlEncodedContent(formData));
+            req.AddParameter("DOC_MODAL_ID__", 0);
+            req.AddParameter("FAST_VERLAST__", _token);
+            var resp = await _client.ExecutePostTaskAsync(req);
             UpdateTokenForNextRequest(resp);
 
-            var respContent = await resp.Content.ReadAsStringAsync();
+            string respContent;
+            // resp.Content can't be directly used for deserialization due to UTF-8 BOM.
+            using (var reader = new StreamReader(new MemoryStream(resp.RawBytes))) {
+                respContent = reader.ReadToEnd();
+            }
             var jsonResult = JsonConvert.DeserializeObject<GstJsonResult>(respContent);
+
             var htmlStr = jsonResult.Updates.FieldUpdates.Last().Value;
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(htmlStr);
@@ -133,9 +136,11 @@ namespace Amry.Gst
                 .ToArray();
         }
 
-        void UpdateTokenForNextRequest(HttpResponseMessage response)
+        void UpdateTokenForNextRequest(IRestResponse response)
         {
-            _token = response.Headers.GetValues("Fast-Ver-Last").First();
+            _token = (string) response.Headers
+                .FirstOrDefault(x => x.Name == "Fast-Ver-Last")
+                .Value;
         }
     }
 }
