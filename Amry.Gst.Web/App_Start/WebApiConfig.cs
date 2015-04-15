c7b30d55cc42e0a48e273085e71abf2584116d75
Shamsul Amry Mokhtar <amry@equadtech.com>
2015-04-15 16:25:58 +0800

    Compress response from Web API

--- Amry.Gst.Web/App_Start/WebApiConfig.cs
+++ Amry.Gst.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Http;
+using Microsoft.AspNet.WebApi.MessageHandlers.Compression;
+using Microsoft.AspNet.WebApi.MessageHandlers.Compression.Compressors;
 
 namespace Amry.Gst.Web
 {
@@ -9,6 +11,8 @@ namespace Amry.Gst.Web
             // Web API configuration and services
             config.Formatters.Remove(config.Formatters.XmlFormatter);
             config.Filters.Add(new WebApiExceptionFilterAttribute());
+            config.MessageHandlers.Insert(0, new ServerCompressionHandler(
+                new GZipCompressor(), new DeflateCompressor()));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
