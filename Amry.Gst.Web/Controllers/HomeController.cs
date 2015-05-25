1408c9df11a50ff058c99b5ce4a2830c9f8a0444
Shamsul Amry Mokhtar <amry@equadtech.com>
2015-05-26 03:06:53 +0800

    Cache main page for one week

--- Amry.Gst.Web/Controllers/HomeController.cs
+++ Amry.Gst.Web/Controllers/HomeController.cs
@@ -6,9 +6,10 @@ namespace Amry.Gst.Web.Controllers
 {
     public class HomeController : Controller
     {
+        const int OneWeek = 604800;
         const int OneYear = 31536000;
 
-        [Route, MinifyHtml]
+        [Route, MinifyHtml, OutputCache(Duration = OneWeek)]
         public ActionResult Index()
         {
             return View();
